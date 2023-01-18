7190d75acafd6d9c9824e6a2780302729619cacc
Lev Rumyantsev <levarum@google.com>
2023-01-17 19:14:18 -0800

    decoder: inception
    Test: berberis_decoder_riscv64_host_test
    Bug: 265372622
    Change-Id: Icbeb73f3c9e23d1c7d2734fcf1a5193849c39266

---
+++ decoder/include/berberis/decoder/riscv64/decoder.h
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef BERBERIS_DECODER_RISCV64_DECODER_H_
+#define BERBERIS_DECODER_RISCV64_DECODER_H_
+
+#include <climits>
+#include <cstdint>
+#include <cstdlib>
+#include <type_traits>
+
+namespace berberis {
+
+// Decode() method takes a sequence of bytes and decodes it into the instruction opcode and fields.
+// The InsnConsumer's method corresponding to the decoded opcode is called with the decoded fields
+// as an argument. Returned is the instruction size.
+template <class InsnConsumer>
+class Decoder {
+ public:
+  explicit Decoder(InsnConsumer* insn_consumer) : insn_consumer_(insn_consumer) {}
+
+  enum class BaseOpcode {
+    kOp = 0b01'100,
+  };
+
+  enum class OpOpcode {
+    kAdd = 0b0000000'000,
+  };
+
+  struct OpArgs {
+    OpOpcode opcode;
+  };
+
+  uint8_t Decode(const uint16_t* code) {
+    constexpr uint16_t kInsnLenMask = uint16_t{0b11};
+    if ((*code & kInsnLenMask) != kInsnLenMask)  {
+      // TODO(b/265372622): Support 16-bit instructions.
+      insn_consumer_->Unimplemented();
+      return 2;
+    }
+
+    code_ = *reinterpret_cast<const uint32_t*>(code);
+
+    BaseOpcode opcode_bits = BaseOpcode{GetBits<uint8_t, 2, 5>()};
+
+    switch (opcode_bits) {
+      case BaseOpcode::kOp:
+        DecodeOp();
+        break;
+      default:
+        insn_consumer_->Unimplemented();
+    }
+    return 4;
+  }
+
+ private:
+  template <typename ResultType, uint32_t start, uint32_t size>
+  ResultType GetBits() {
+    static_assert(std::is_unsigned_v<ResultType>, "Only unsigned types are supported");
+    static_assert(sizeof(ResultType) * CHAR_BIT >= size, "Too small ResultType for size");
+    static_assert((start + size) <= 32 && size > 0, "Invalid start or size value");
+    uint32_t shifted_val = code_ << (32 - start - size);
+    return static_cast<ResultType>(shifted_val >> (32 - size));
+  }
+
+  void DecodeOp() {
+    uint16_t low_opcode = GetBits<uint16_t, 12, 3>();
+    uint16_t high_opcode = GetBits<uint16_t, 25, 7>();
+    OpOpcode opcode = OpOpcode{low_opcode | (high_opcode << 3)};
+    const OpArgs args = {
+        .opcode = opcode,
+    };
+    insn_consumer_->Op(args);
+  }
+
+  InsnConsumer* insn_consumer_;
+  uint32_t code_;
+};
+
+}  // namespace berberis
+
+#endif  // BERBERIS_DECODER_RISCV64_DECODER_H_
