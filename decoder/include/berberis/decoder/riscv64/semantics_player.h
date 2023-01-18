dcd428749d896a675a91a79f44f8d2854101ae78
Lev Rumyantsev <levarum@google.com>
2023-01-17 19:14:18 -0800

    Add SemanticsPlayer and Interpreter
    Test: berberis_interpreter_riscv64_host_test
    Bug: 265372622
    Change-Id: I7d76e20e7e37413c945e2431e5506a1fba4c4c8f

---
+++ decoder/include/berberis/decoder/riscv64/semantics_player.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef BERBERIS_DECODER_RISCV64_SEMANTICS_PLAYER_H_
+#define BERBERIS_DECODER_RISCV64_SEMANTICS_PLAYER_H_
+
+#include "berberis/decoder/riscv64/decoder.h"
+
+namespace berberis {
+
+// This class expresses the semantics of instructions by calling a sequence of SemanticsListener
+// callbacks.
+template <class SemanticsListener>
+class SemanticsPlayer {
+ public:
+  using Decoder = Decoder<SemanticsPlayer>;
+  using Register = typename SemanticsListener::Register;
+
+  explicit SemanticsPlayer(SemanticsListener* listener) : listener_(listener) {}
+
+  // Decoder's InsnConsumer implementation.
+  void Op(const typename Decoder::OpArgs& args) {
+    Register arg1 = GetRegOrZero(args.src1);
+    Register arg2 = GetRegOrZero(args.src2);
+    Register result = listener_->Op(args.opcode, arg1, arg2);
+    SetRegOrIgnore(args.dst, result);
+  };
+
+  void Unimplemented() {
+    listener_->Unimplemented();
+  };
+
+ private:
+  Register GetRegOrZero(uint8_t reg) {
+    return reg == 0 ? listener_->GetImm(0) : listener_->GetReg(reg);
+  }
+
+  void SetRegOrIgnore(uint8_t reg, Register value) {
+    if (reg != 0) {
+      listener_->SetReg(reg, value);
+    }
+  }
+
+  SemanticsListener* listener_;
+};
+
+}  // namespace berberis
+
+#endif  // BERBERIS_DECODER_RISCV64_SEMANTICS_PLAYER_H_
