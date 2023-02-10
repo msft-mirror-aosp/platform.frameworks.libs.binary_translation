d1cf46ccc9031a885a9fc2b6477db1ec7dae5806
Lev Rumyantsev <levarum@google.com>
2023-02-10 12:25:41 -0800

    berberis: aosp inception
    Test: tree-hugger
    Bug: 265372622
    Change-Id: I57d1c3d7f6656a868591035eccabbd39849cb78b

---
+++ interpreter/riscv64/interpreter.cc
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "berberis/interpreter/riscv64/interpreter.h"
+
+#include <cstdint>
+
+#include "berberis/base/bit_util.h"
+#include "berberis/base/checks.h"
+#include "berberis/base/logging.h"
+#include "berberis/base/macros.h"
+#include "berberis/decoder/riscv64/decoder.h"
+#include "berberis/decoder/riscv64/semantics_player.h"
+#include "berberis/guest_state/guest_state_riscv64.h"
+
+namespace berberis {
+
+namespace {
+
+class Interpreter {
+ public:
+  using Decoder = Decoder<SemanticsPlayer<Interpreter>>;
+  using Register = uint64_t;
+
+  explicit Interpreter(ThreadState* state)
+      : state_(state) {}
+
+  //
+  // Instruction implementations.
+  //
+
+  Register Op(Decoder::OpOpcode opcode, Register arg1, Register arg2) {
+    switch (opcode) {
+      case Decoder::OpOpcode::kAdd:
+        return arg1 + arg2;
+      case Decoder::OpOpcode::kSub:
+        return arg1 - arg2;
+      case Decoder::OpOpcode::kAnd:
+        return arg1 & arg2;
+      case Decoder::OpOpcode::kOr:
+        return arg1 | arg2;
+      case Decoder::OpOpcode::kXor:
+        return arg1 ^ arg2;
+      case Decoder::OpOpcode::kSll:
+        return arg1 << arg2;
+      case Decoder::OpOpcode::kSlr:
+        return arg1 >> arg2;
+      case Decoder::OpOpcode::kSra:
+        return bit_cast<int64_t>(arg1) >> arg2;
+      case Decoder::OpOpcode::kSlt:
+        return bit_cast<int64_t>(arg1) < bit_cast<int64_t>(arg2) ? 1 : 0;
+      case Decoder::OpOpcode::kSltu:
+        return arg1 < arg2 ? 1 : 0;
+      default:
+        Unimplemented();
+        break;
+    }
+  }
+
+  void Unimplemented() {
+    LOG_ALWAYS_FATAL("Unimplemented riscv64 instruction");
+  }
+
+  //
+  // Guest state getters/setters.
+  //
+
+  uint64_t GetReg(uint8_t reg) const {
+    CheckRegIsValid(reg);
+    return state_->cpu.x[reg - 1];
+  }
+
+  void SetReg(uint8_t reg, Register value) {
+    CheckRegIsValid(reg);
+    state_->cpu.x[reg - 1] = value;
+  }
+
+  //
+  // Various helper methods.
+  //
+
+  uint64_t GetImm(uint64_t imm) const { return imm; }
+
+  void FinalizeInsn(uint8_t insn_len) {
+      state_->cpu.insn_addr += insn_len;
+  }
+
+ private:
+  void CheckRegIsValid(uint8_t reg) const {
+    CHECK_GT(reg, 0u);
+    CHECK_LE(reg, arraysize(state_->cpu.x));
+  }
+
+  ThreadState* state_;
+};
+
+}  // namespace
+
+void InterpretInsn(ThreadState* state) {
+  GuestAddr pc = state->cpu.insn_addr;
+
+  Interpreter interpreter(state);
+  SemanticsPlayer sem_player(&interpreter);
+  Decoder decoder(&sem_player);
+  uint8_t insn_len = decoder.Decode(bit_cast<const uint16_t*>(pc));
+  interpreter.FinalizeInsn(insn_len);
+}
+
+}  // namespace berberis
---
+++ interpreter/riscv64/interpreter_test.cc
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "gtest/gtest.h"
+
+#include <initializer_list>
+#include <tuple>
+
+#include "berberis/base/bit_util.h"
+#include "berberis/guest_state/guest_state_riscv64.h"
+#include "berberis/interpreter/riscv64/interpreter.h"
+
+namespace berberis {
+
+namespace {
+
+class Riscv64InterpreterTest : public ::testing::Test {
+ public:
+  void InterpretOp(uint32_t insn_bytes,
+                   // The tuple is [arg1, arg2, expected_result].
+                   std::initializer_list<std::tuple<uint64_t, uint64_t, uint64_t>> args) {
+    for (auto arg : args) {
+      state_.cpu.insn_addr = bit_cast<GuestAddr>(&insn_bytes);
+      SetXReg<2>(state_.cpu, std::get<0>(arg));
+      SetXReg<3>(state_.cpu, std::get<1>(arg));
+      InterpretInsn(&state_);
+      EXPECT_EQ(GetXReg<1>(state_.cpu), std::get<2>(arg));
+    }
+  }
+ protected:
+  ThreadState state_;
+};
+
+TEST_F(Riscv64InterpreterTest, OpInstructions) {
+  // Add
+  InterpretOp(0x003100b3, {{19, 23, 42}});
+  // Sub
+  InterpretOp(0x403100b3, {{42, 23, 19}});
+  // And
+  InterpretOp(0x003170b3, {{0b0101, 0b0011, 0b0001}});
+  // Or
+  InterpretOp(0x003160b3, {{0b0101, 0b0011, 0b0111}});
+  // Xor
+  InterpretOp(0x003140b3, {{0b0101, 0b0011, 0b0110}});
+  // Sll
+  InterpretOp(0x003110b3, {{0b1010, 3, 0b1010'000}});
+  // Slr
+  InterpretOp(0x003150b3, {{0xf000'0000'0000'0000ULL, 12, 0x000f'0000'0000'0000ULL}});
+  // Sla
+  InterpretOp(0x403150b3, {{0xf000'0000'0000'0000ULL, 12, 0xffff'0000'0000'0000ULL}});
+  // Slt
+  InterpretOp(0x003120b3, {
+    {19, 23, 1},
+    {23, 19, 0},
+    {~0ULL, 0, 1},
+  });
+  // Sltu
+  InterpretOp(0x003130b3, {
+    {19, 23, 1},
+    {23, 19, 0},
+    {~0ULL, 0, 0},
+  });
+}
+
+}  // namespace
+
+}  // namespace berberis
