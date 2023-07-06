613df3cd9d0c381f594dcd600fd8cc2f0d12652d
Victor Khimenko <khim@google.com>
2023-07-06 16:04:46 +0000

    Add CanonicalizeNan assembler intrinsic.
    Bug: 278812060
    
    Test: m berberis_all
    
    Change-Id: I9acf68bf0edf98130b2991c65d3b1b8d82d4a406

--- intrinsics/riscv64_to_x86_64/include/berberis/intrinsics/macro_assembler-impl.h
+++ intrinsics/riscv64_to_x86_64/include/berberis/intrinsics/macro_assembler-impl.h
@@ -24,34 +24,69 @@ namespace berberis {
 
 namespace constants_pool {
 
-// Constant suitable for NaNBoxing of RISC-V 32bit float with PXor.
-extern const int32_t kNanBoxFloat32;
-extern const int32_t kNanBoxedNansFloat32;
+// Constant suitable for NaN boxing of RISC-V 32bit float with PXor.
+// Note: technically we only need to Nan-box Float32 since we don't support Float16 yet.
+template <typename FloatType>
+extern const int32_t kNanBox;
+template <>
+extern const int32_t kNanBox<intrinsics::Float32>;
+template <typename FloatType>
+extern const int32_t kNanBoxedNans;
+template <>
+extern const int32_t kNanBoxedNans<intrinsics::Float32>;
+template <typename FloatType>
+extern const int32_t kCanonicalNans;
+
+// Canonical NaNs. Float32 and Float64 are supported.
+template <>
+extern const int32_t kCanonicalNans<intrinsics::Float32>;
+template <>
+extern const int32_t kCanonicalNans<intrinsics::Float64>;
 
 }  // namespace constants_pool
 
 template <typename Assembler>
 template <typename FloatType>
+void MacroAssembler<Assembler>::CanonicalizeNan(XMMRegister result, XMMRegister src) {
+  Pmov(result, src);
+  Cmpords<FloatType>(result, src);
+  Pand(src, result);
+  Pandn(result, {.disp = constants_pool::kCanonicalNans<FloatType>});
+  Por(result, src);
+}
+
+template <typename Assembler>
+template <typename FloatType>
+void MacroAssembler<Assembler>::CanonicalizeNanAVX(XMMRegister result, XMMRegister src) {
+  Vcmpords<FloatType>(result, src, src);
+  Vpand(src, src, result);
+  Vpandn(result, result, {.disp = constants_pool::kCanonicalNans<FloatType>});
+  Vpor(result, result, src);
+}
+
+template <typename Assembler>
+template <typename FloatType>
 void MacroAssembler<Assembler>::MacroUnboxNan(XMMRegister result, XMMRegister src) {
-  static_assert(std::is_same_v<FloatType, intrinsics::Float32>);
+  static_assert(std::is_same_v<FloatType, Float32>);
 
   Pmov(result, src);
-  Pcmpeq<typename TypeTraits<FloatType>::Int>(result, {.disp = constants_pool::kNanBoxFloat32});
+  Pcmpeq<typename TypeTraits<FloatType>::Int>(result, {.disp = constants_pool::kNanBox<Float32>});
   Pshufd(result, result, kShuffleDDBB);
   Pand(src, result);
-  Pandn(result, {.disp = constants_pool::kNanBoxedNansFloat32});
+  Pandn(result, {.disp = constants_pool::kNanBoxedNans<Float32>});
   Por(result, src);
 }
 
 template <typename Assembler>
 template <typename FloatType>
 void MacroAssembler<Assembler>::MacroUnboxNanAVX(XMMRegister result, XMMRegister src) {
-  static_assert(std::is_same_v<FloatType, intrinsics::Float32>);
+  static_assert(std::is_same_v<FloatType, Float32>);
 
-  Vpcmpeq<typename TypeTraits<FloatType>::Int>(result, src, {.disp = constants_pool::kNanBoxFloat32});
+  Vpcmpeq<typename TypeTraits<FloatType>::Int>(
+      result, src, {.disp = constants_pool::kNanBox<Float32>});
   Vpshufd(result, result, kShuffleDDBB);
   Vpand(src, src, result);
-  Vpandn(result, result, {.disp = constants_pool::kNanBoxedNansFloat32});
+  Vpandn(result, result, {.disp = constants_pool::kNanBoxedNans<Float32>});
   Vpor(result, result, src);
 }
 
