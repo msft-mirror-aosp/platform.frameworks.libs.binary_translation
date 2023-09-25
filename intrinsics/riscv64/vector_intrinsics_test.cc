e772f5dd13928ddc9c24615257bedf3a0d0a1165
Victor Khimenko <khim@google.com>
2023-09-25 15:37:29 +0000

    Add test for vl for intrinsics
    Bug: 295216367
    
    Test: m berberis_all
    
    Change-Id: I046e4bf08fbfc8e9e4cefd7a4339125458e53157

--- intrinsics/riscv64/vector_intrinsics_test.cc
+++ intrinsics/riscv64/vector_intrinsics_test.cc
@@ -111,6 +111,208 @@ TEST(VectorIntrinsics, Vaddvx) {
          __v2du{0x0000'0000'0000'0000, 0xffff'ffff'ffff'ffff});
 }
 
+TEST(VectorIntrinsics, VlArgForVv) {
+  auto Verify = [](auto Vaddvva,
+                   auto Vaddvvu,
+                   auto Vaddvvmaa,
+                   auto Vaddvvmau,
+                   auto Vaddvvmua,
+                   auto Vaddvvmuu,
+                   SIMD128Register arg2,
+                   auto result_to_check_agnostic,
+                   auto result_to_check_undisturbed) {
+    static_assert(
+        std::is_same_v<decltype(result_to_check_agnostic), decltype(result_to_check_undisturbed)>);
+    constexpr size_t kHalfLen =
+        sizeof(result_to_check_agnostic) / sizeof(result_to_check_agnostic[0]) / 2;
+    ASSERT_EQ(Vaddvva(0, kHalfLen, kUndisturbedResult, __m128i{-1, -1}, arg2),
+              std::tuple{result_to_check_agnostic});
+    ASSERT_EQ(Vaddvvu(0, kHalfLen, kUndisturbedResult, __m128i{-1, -1}, arg2),
+              std::tuple{result_to_check_undisturbed});
+    ASSERT_EQ(Vaddvvmaa(0, kHalfLen, 0xffff, kUndisturbedResult, __m128i{-1, -1}, arg2),
+              std::tuple{result_to_check_agnostic});
+    ASSERT_EQ(Vaddvvmau(0, kHalfLen, 0xffff, kUndisturbedResult, __m128i{-1, -1}, arg2),
+              std::tuple{result_to_check_agnostic});
+    ASSERT_EQ(Vaddvvmua(0, kHalfLen, 0xffff, kUndisturbedResult, __m128i{-1, -1}, arg2),
+              std::tuple{result_to_check_undisturbed});
+    ASSERT_EQ(Vaddvvmuu(0, kHalfLen, 0xffff, kUndisturbedResult, __m128i{-1, -1}, arg2),
+              std::tuple{result_to_check_undisturbed});
+  };
+  Verify(Vaddvv<uint8_t, TailProcessing::kAgnostic>,
+         Vaddvv<uint8_t, TailProcessing::kUndisturbed>,
+         Vaddvvm<uint8_t, TailProcessing::kAgnostic, InactiveProcessing::kAgnostic>,
+         Vaddvvm<uint8_t, TailProcessing::kAgnostic, InactiveProcessing::kUndisturbed>,
+         Vaddvvm<uint8_t, TailProcessing::kUndisturbed, InactiveProcessing::kAgnostic>,
+         Vaddvvm<uint8_t, TailProcessing::kUndisturbed, InactiveProcessing::kUndisturbed>,
+         __v16qu{0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+         __v16qu{255, 0, 255, 0, 255, 0, 255, 0, 255, 255, 255, 255, 255, 255, 255, 255},
+         __v16qu{255, 0, 255, 0, 255, 0, 255, 0, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55});
+  Verify(Vaddvv<uint8_t, TailProcessing::kAgnostic>,
+         Vaddvv<uint8_t, TailProcessing::kUndisturbed>,
+         Vaddvvm<uint8_t, TailProcessing::kAgnostic, InactiveProcessing::kAgnostic>,
+         Vaddvvm<uint8_t, TailProcessing::kAgnostic, InactiveProcessing::kUndisturbed>,
+         Vaddvvm<uint8_t, TailProcessing::kUndisturbed, InactiveProcessing::kAgnostic>,
+         Vaddvvm<uint8_t, TailProcessing::kUndisturbed, InactiveProcessing::kUndisturbed>,
+         __v16qu{1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+         __v16qu{0, 255, 0, 255, 0, 255, 0, 255, 255, 255, 255, 255, 255, 255, 255, 255},
+         __v16qu{0, 255, 0, 255, 0, 255, 0, 255, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55});
+  Verify(Vaddvv<uint16_t, TailProcessing::kAgnostic>,
+         Vaddvv<uint16_t, TailProcessing::kUndisturbed>,
+         Vaddvvm<uint16_t, TailProcessing::kAgnostic, InactiveProcessing::kAgnostic>,
+         Vaddvvm<uint16_t, TailProcessing::kAgnostic, InactiveProcessing::kUndisturbed>,
+         Vaddvvm<uint16_t, TailProcessing::kUndisturbed, InactiveProcessing::kAgnostic>,
+         Vaddvvm<uint16_t, TailProcessing::kUndisturbed, InactiveProcessing::kUndisturbed>,
+         __v8hu{0, 1, 0, 1, 0, 1, 0, 1},
+         __v8hu{0xffff, 0x0000, 0xffff, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff},
+         __v8hu{0xffff, 0x0000, 0xffff, 0x0000, 0x5555, 0x5555, 0x5555, 0x5555});
+  Verify(Vaddvv<uint16_t, TailProcessing::kAgnostic>,
+         Vaddvv<uint16_t, TailProcessing::kUndisturbed>,
+         Vaddvvm<uint16_t, TailProcessing::kAgnostic, InactiveProcessing::kAgnostic>,
+         Vaddvvm<uint16_t, TailProcessing::kAgnostic, InactiveProcessing::kUndisturbed>,
+         Vaddvvm<uint16_t, TailProcessing::kUndisturbed, InactiveProcessing::kAgnostic>,
+         Vaddvvm<uint16_t, TailProcessing::kUndisturbed, InactiveProcessing::kUndisturbed>,
+         __v8hu{1, 0, 1, 0, 1, 0, 1, 0},
+         __v8hu{0x0000, 0xffff, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff},
+         __v8hu{0x0000, 0xffff, 0x0000, 0xffff, 0x5555, 0x5555, 0x5555, 0x5555});
+  Verify(Vaddvv<uint32_t, TailProcessing::kAgnostic>,
+         Vaddvv<uint32_t, TailProcessing::kUndisturbed>,
+         Vaddvvm<uint32_t, TailProcessing::kAgnostic, InactiveProcessing::kAgnostic>,
+         Vaddvvm<uint32_t, TailProcessing::kAgnostic, InactiveProcessing::kUndisturbed>,
+         Vaddvvm<uint32_t, TailProcessing::kUndisturbed, InactiveProcessing::kAgnostic>,
+         Vaddvvm<uint32_t, TailProcessing::kUndisturbed, InactiveProcessing::kUndisturbed>,
+         __v4su{0, 1, 0, 1},
+         __v4su{0xffff'ffff, 0x0000'0000, 0xffff'ffff, 0xffff'ffff},
+         __v4su{0xffff'ffff, 0x0000'0000, 0x5555'5555, 0x5555'5555});
+  Verify(Vaddvv<uint32_t, TailProcessing::kAgnostic>,
+         Vaddvv<uint32_t, TailProcessing::kUndisturbed>,
+         Vaddvvm<uint32_t, TailProcessing::kAgnostic, InactiveProcessing::kAgnostic>,
+         Vaddvvm<uint32_t, TailProcessing::kAgnostic, InactiveProcessing::kUndisturbed>,
+         Vaddvvm<uint32_t, TailProcessing::kUndisturbed, InactiveProcessing::kAgnostic>,
+         Vaddvvm<uint32_t, TailProcessing::kUndisturbed, InactiveProcessing::kUndisturbed>,
+         __v4su{1, 0, 1, 0},
+         __v4su{0x0000'0000, 0xffff'ffff, 0xffff'ffff, 0xffff'ffff},
+         __v4su{0x0000'0000, 0xffff'ffff, 0x5555'5555, 0x5555'5555});
+  Verify(Vaddvv<uint64_t, TailProcessing::kAgnostic>,
+         Vaddvv<uint64_t, TailProcessing::kUndisturbed>,
+         Vaddvvm<uint64_t, TailProcessing::kAgnostic, InactiveProcessing::kAgnostic>,
+         Vaddvvm<uint64_t, TailProcessing::kAgnostic, InactiveProcessing::kUndisturbed>,
+         Vaddvvm<uint64_t, TailProcessing::kUndisturbed, InactiveProcessing::kAgnostic>,
+         Vaddvvm<uint64_t, TailProcessing::kUndisturbed, InactiveProcessing::kUndisturbed>,
+         __v2du{0, 1},
+         __v2du{0xffff'ffff'ffff'ffff, 0xffff'ffff'ffff'ffff},
+         __v2du{0xffff'ffff'ffff'ffff, 0x5555'5555'5555'5555});
+  Verify(Vaddvv<uint64_t, TailProcessing::kAgnostic>,
+         Vaddvv<uint64_t, TailProcessing::kUndisturbed>,
+         Vaddvvm<uint64_t, TailProcessing::kAgnostic, InactiveProcessing::kAgnostic>,
+         Vaddvvm<uint64_t, TailProcessing::kAgnostic, InactiveProcessing::kUndisturbed>,
+         Vaddvvm<uint64_t, TailProcessing::kUndisturbed, InactiveProcessing::kAgnostic>,
+         Vaddvvm<uint64_t, TailProcessing::kUndisturbed, InactiveProcessing::kUndisturbed>,
+         __v2du{1, 0},
+         __v2du{0x0000'0000'0000'0000, 0xffff'ffff'ffff'ffff},
+         __v2du{0x0000'0000'0000'0000, 0x5555'5555'5555'5555});
+}
+
+TEST(VectorIntrinsics, VlArgForVx) {
+  auto Verify = [](auto Vaddvxa,
+                   auto Vaddvxu,
+                   auto Vaddvxmaa,
+                   auto Vaddvxmau,
+                   auto Vaddvxmua,
+                   auto Vaddvxmuu,
+                   SIMD128Register arg1,
+                   auto result_to_check_agnostic,
+                   auto result_to_check_undisturbed) {
+    static_assert(
+        std::is_same_v<decltype(result_to_check_agnostic), decltype(result_to_check_undisturbed)>);
+    constexpr size_t kHalfLen =
+        sizeof(result_to_check_agnostic) / sizeof(result_to_check_agnostic[0]) / 2;
+    ASSERT_EQ(Vaddvxa(0, kHalfLen, kUndisturbedResult, arg1, 1),
+              std::tuple{result_to_check_agnostic});
+    ASSERT_EQ(Vaddvxu(0, kHalfLen, kUndisturbedResult, arg1, 1),
+              std::tuple{result_to_check_undisturbed});
+    ASSERT_EQ(Vaddvxmaa(0, kHalfLen, 0xffff, kUndisturbedResult, arg1, 1),
+              std::tuple{result_to_check_agnostic});
+    ASSERT_EQ(Vaddvxmau(0, kHalfLen, 0xffff, kUndisturbedResult, arg1, 1),
+              std::tuple{result_to_check_agnostic});
+    ASSERT_EQ(Vaddvxmua(0, kHalfLen, 0xffff, kUndisturbedResult, arg1, 1),
+              std::tuple{result_to_check_undisturbed});
+    ASSERT_EQ(Vaddvxmuu(0, kHalfLen, 0xffff, kUndisturbedResult, arg1, 1),
+              std::tuple{result_to_check_undisturbed});
+  };
+  Verify(Vaddvx<uint8_t, TailProcessing::kAgnostic>,
+         Vaddvx<uint8_t, TailProcessing::kUndisturbed>,
+         Vaddvxm<uint8_t, TailProcessing::kAgnostic, InactiveProcessing::kAgnostic>,
+         Vaddvxm<uint8_t, TailProcessing::kAgnostic, InactiveProcessing::kUndisturbed>,
+         Vaddvxm<uint8_t, TailProcessing::kUndisturbed, InactiveProcessing::kAgnostic>,
+         Vaddvxm<uint8_t, TailProcessing::kUndisturbed, InactiveProcessing::kUndisturbed>,
+         __v16qu{254, 255, 254, 255, 254, 255, 254, 255, 254, 255, 254, 255, 254, 255, 254, 255},
+         __v16qu{255, 0, 255, 0, 255, 0, 255, 0, 255, 255, 255, 255, 255, 255, 255, 255},
+         __v16qu{255, 0, 255, 0, 255, 0, 255, 0, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55});
+  Verify(Vaddvx<uint8_t, TailProcessing::kAgnostic>,
+         Vaddvx<uint8_t, TailProcessing::kUndisturbed>,
+         Vaddvxm<uint8_t, TailProcessing::kAgnostic, InactiveProcessing::kAgnostic>,
+         Vaddvxm<uint8_t, TailProcessing::kAgnostic, InactiveProcessing::kUndisturbed>,
+         Vaddvxm<uint8_t, TailProcessing::kUndisturbed, InactiveProcessing::kAgnostic>,
+         Vaddvxm<uint8_t, TailProcessing::kUndisturbed, InactiveProcessing::kUndisturbed>,
+         __v16qu{255, 254, 255, 254, 255, 254, 255, 254, 255, 254, 255, 254, 255, 254, 255, 254},
+         __v16qu{0, 255, 0, 255, 0, 255, 0, 255, 255, 255, 255, 255, 255, 255, 255, 255},
+         __v16qu{0, 255, 0, 255, 0, 255, 0, 255, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55});
+  Verify(Vaddvx<uint16_t, TailProcessing::kAgnostic>,
+         Vaddvx<uint16_t, TailProcessing::kUndisturbed>,
+         Vaddvxm<uint16_t, TailProcessing::kAgnostic, InactiveProcessing::kAgnostic>,
+         Vaddvxm<uint16_t, TailProcessing::kAgnostic, InactiveProcessing::kUndisturbed>,
+         Vaddvxm<uint16_t, TailProcessing::kUndisturbed, InactiveProcessing::kAgnostic>,
+         Vaddvxm<uint16_t, TailProcessing::kUndisturbed, InactiveProcessing::kUndisturbed>,
+         __v8hu{0xfffe, 0xffff, 0xfffe, 0xffff, 0xfffe, 0xffff, 0xfffe, 0xffff},
+         __v8hu{0xffff, 0x0000, 0xffff, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff},
+         __v8hu{0xffff, 0x0000, 0xffff, 0x0000, 0x5555, 0x5555, 0x5555, 0x5555});
+  Verify(Vaddvx<uint16_t, TailProcessing::kAgnostic>,
+         Vaddvx<uint16_t, TailProcessing::kUndisturbed>,
+         Vaddvxm<uint16_t, TailProcessing::kAgnostic, InactiveProcessing::kAgnostic>,
+         Vaddvxm<uint16_t, TailProcessing::kAgnostic, InactiveProcessing::kUndisturbed>,
+         Vaddvxm<uint16_t, TailProcessing::kUndisturbed, InactiveProcessing::kAgnostic>,
+         Vaddvxm<uint16_t, TailProcessing::kUndisturbed, InactiveProcessing::kUndisturbed>,
+         __v8hu{0xffff, 0xfffe, 0xffff, 0xfffe, 0xffff, 0xfffe, 0xffff, 0xfffe},
+         __v8hu{0x0000, 0xffff, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff},
+         __v8hu{0x0000, 0xffff, 0x0000, 0xffff, 0x5555, 0x5555, 0x5555, 0x5555});
+  Verify(Vaddvx<uint32_t, TailProcessing::kAgnostic>,
+         Vaddvx<uint32_t, TailProcessing::kUndisturbed>,
+         Vaddvxm<uint32_t, TailProcessing::kAgnostic, InactiveProcessing::kAgnostic>,
+         Vaddvxm<uint32_t, TailProcessing::kAgnostic, InactiveProcessing::kUndisturbed>,
+         Vaddvxm<uint32_t, TailProcessing::kUndisturbed, InactiveProcessing::kAgnostic>,
+         Vaddvxm<uint32_t, TailProcessing::kUndisturbed, InactiveProcessing::kUndisturbed>,
+         __v4su{0xffff'fffe, 0xffff'ffff, 0xffff'fffe, 0xffff'ffff},
+         __v4su{0xffff'ffff, 0x0000'0000, 0xffff'ffff, 0xffff'ffff},
+         __v4su{0xffff'ffff, 0x0000'0000, 0x5555'5555, 0x5555'5555});
+  Verify(Vaddvx<uint32_t, TailProcessing::kAgnostic>,
+         Vaddvx<uint32_t, TailProcessing::kUndisturbed>,
+         Vaddvxm<uint32_t, TailProcessing::kAgnostic, InactiveProcessing::kAgnostic>,
+         Vaddvxm<uint32_t, TailProcessing::kAgnostic, InactiveProcessing::kUndisturbed>,
+         Vaddvxm<uint32_t, TailProcessing::kUndisturbed, InactiveProcessing::kAgnostic>,
+         Vaddvxm<uint32_t, TailProcessing::kUndisturbed, InactiveProcessing::kUndisturbed>,
+         __v4su{0xffff'ffff, 0xffff'fffe, 0xffff'ffff, 0xffff'fffe},
+         __v4su{0x0000'0000, 0xffff'ffff, 0xffff'ffff, 0xffff'ffff},
+         __v4su{0x0000'0000, 0xffff'ffff, 0x5555'5555, 0x5555'5555});
+  Verify(Vaddvx<uint64_t, TailProcessing::kAgnostic>,
+         Vaddvx<uint64_t, TailProcessing::kUndisturbed>,
+         Vaddvxm<uint64_t, TailProcessing::kAgnostic, InactiveProcessing::kAgnostic>,
+         Vaddvxm<uint64_t, TailProcessing::kAgnostic, InactiveProcessing::kUndisturbed>,
+         Vaddvxm<uint64_t, TailProcessing::kUndisturbed, InactiveProcessing::kAgnostic>,
+         Vaddvxm<uint64_t, TailProcessing::kUndisturbed, InactiveProcessing::kUndisturbed>,
+         __v2du{0xffff'ffff'ffff'fffe, 0xffff'ffff'ffff'ffff},
+         __v2du{0xffff'ffff'ffff'ffff, 0xffff'ffff'ffff'ffff},
+         __v2du{0xffff'ffff'ffff'ffff, 0x5555'5555'5555'5555});
+  Verify(Vaddvx<uint64_t, TailProcessing::kAgnostic>,
+         Vaddvx<uint64_t, TailProcessing::kUndisturbed>,
+         Vaddvxm<uint64_t, TailProcessing::kAgnostic, InactiveProcessing::kAgnostic>,
+         Vaddvxm<uint64_t, TailProcessing::kAgnostic, InactiveProcessing::kUndisturbed>,
+         Vaddvxm<uint64_t, TailProcessing::kUndisturbed, InactiveProcessing::kAgnostic>,
+         Vaddvxm<uint64_t, TailProcessing::kUndisturbed, InactiveProcessing::kUndisturbed>,
+         __v2du{0xffff'ffff'ffff'ffff, 0xffff'ffff'ffff'fffe},
+         __v2du{0x0000'0000'0000'0000, 0xffff'ffff'ffff'ffff},
+         __v2du{0x0000'0000'0000'0000, 0x5555'5555'5555'5555});
+}
+
 TEST(VectorIntrinsics, VstartArgVv) {
   auto Verify = [](auto Vaddvva,
                    auto Vaddvvu,
