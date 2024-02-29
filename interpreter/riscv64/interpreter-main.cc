810b0e26df66029d06ef2bafc92a8f6ad4cfbe00
Victor Khimenko <khim@google.com>
2024-02-29 19:57:46 +0000

    Move instantiation of vector instructions into 14 files.
    If -DBERBERIS_RISCV64_INTERPRETER_SEPARATE_INSTANTIATION_OF_VECTOR_OPERATIONS
    is removed from interpreter/Android.bp instantiation is moved to one file,
    like before.
    
    Test:  berberis_all
    
    Change-Id: I3d04808f26198e4159b7d997db2fa521db4d48a9

---
+++ interpreter/riscv64/interpreter-main.cc
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file excenaupt in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "berberis/interpreter/riscv64/interpreter.h"
+
+#include "berberis/decoder/riscv64/decoder.h"
+#include "berberis/decoder/riscv64/semantics_player.h"
+#include "berberis/guest_state/guest_addr.h"
+#include "berberis/guest_state/guest_state.h"
+
+#include "faulty_memory_accesses.h"
+#include "interpreter.h"
+
+namespace berberis {
+
+void InitInterpreter() {
+  AddFaultyMemoryAccessRecoveryCode();
+}
+
+void InterpretInsn(ThreadState* state) {
+  GuestAddr pc = state->cpu.insn_addr;
+
+  Interpreter interpreter(state);
+  SemanticsPlayer sem_player(&interpreter);
+  Decoder decoder(&sem_player);
+  uint8_t insn_len = decoder.Decode(ToHostAddr<const uint16_t>(pc));
+  interpreter.FinalizeInsn(insn_len);
+}
+
+}  // namespace berberis
