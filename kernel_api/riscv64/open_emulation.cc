8312a008f589d4083f5cffbe5c5c34e1e391428f
Lev Rumyantsev <levarum@google.com>
2024-08-19 21:59:03 -0700

    19-aug-24: Upstream changes
    Flag: EXEMPT NDK
    Test: tree-hugger
    Bug: None
    Change-Id: Ie13f30184f536d9e1b9e0116d87b828a7703cb82

--- kernel_api/riscv64/open_emulation.cc
+++ kernel_api/riscv64/open_emulation.cc
@@ -23,14 +23,19 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-#include "berberis/kernel_api/tracing.h"
-
-#define GUEST_O_LARGEFILE 00100000
+#include "berberis/base/tracing.h"
 
 namespace berberis {
 
-#if !defined(__i386__) && !defined(__x86_64__)
-#error Currently open flags conversion is only supported on x86
+#if !defined(__x86_64__)
+#error Currently open flags conversion is only supported on x86_64
+#endif
+
+// Glibc doesn't support O_LARGEFILE and defines it to 0. Here we need
+// kernel's definition for x86_64.
+#if (O_LARGEFILE == 0)
+#undef O_LARGEFILE
+#define O_LARGEFILE 00100000
 #endif
 
 // Glibc doesn't expose __O_SYNC
@@ -69,53 +74,38 @@ static_assert(O_DIRECT == 040000);
 static_assert(__O_SYNC == 04000000);
 static_assert(O_SYNC == (O_DSYNC | __O_SYNC));
 static_assert(O_PATH == 010000000);
+static_assert(O_LARGEFILE == 00100000);
 
 namespace {
 
-const int kCompatibleOpenFlags = O_ACCMODE | O_CREAT | O_EXCL | O_NOCTTY | O_TRUNC | O_APPEND |
-                                 O_NONBLOCK | O_DSYNC | FASYNC | O_NOATIME | O_DIRECTORY |
-                                 O_NOFOLLOW | O_CLOEXEC | O_DIRECT | __O_SYNC | O_PATH;
+const int kCompatibleOpenFlags =
+    O_ACCMODE | O_CREAT | O_EXCL | O_NOCTTY | O_TRUNC | O_APPEND | O_NONBLOCK | O_DSYNC | FASYNC |
+    O_NOATIME | O_DIRECTORY | O_NOFOLLOW | O_CLOEXEC | O_DIRECT | __O_SYNC | O_PATH | O_LARGEFILE;
 
 }  // namespace
 
 const char* kGuestCpuinfoPath = "/system/etc/cpuinfo.riscv64.txt";
 
 int ToHostOpenFlags(int guest_flags) {
-  const int kIncompatibleGuestOpenFlags = GUEST_O_LARGEFILE;
-
-  int unknown_guest_flags = guest_flags & ~(kCompatibleOpenFlags | kIncompatibleGuestOpenFlags);
+  int unknown_guest_flags = guest_flags & ~kCompatibleOpenFlags;
   if (unknown_guest_flags) {
-    KAPI_TRACE("Unsupported guest open flags: original=0x%x unsupported=0x%x",
-               guest_flags,
-               unknown_guest_flags);
-  }
-
-  int host_flags = guest_flags & ~kIncompatibleGuestOpenFlags;
-
-  if (guest_flags & GUEST_O_LARGEFILE) {
-    host_flags |= O_LARGEFILE;
-  }
-
-  return host_flags;
-}
-
-int ToGuestOpenFlags(int host_flags) {
-  const int kIncompatibleHostOpenFlags = O_LARGEFILE;
-
-  int unknown_host_flags = host_flags & ~(kCompatibleOpenFlags | kIncompatibleHostOpenFlags);
-  if (unknown_host_flags) {
-    KAPI_TRACE("Unsupported host open flags: original=0x%x unsupported=0x%x",
-               host_flags,
-               unknown_host_flags);
-  }
-
-  int guest_flags = host_flags & ~kIncompatibleHostOpenFlags;
-
-  if (host_flags & O_LARGEFILE) {
-    guest_flags |= GUEST_O_LARGEFILE;
+    TRACE("Unrecognized guest open flags: original=0x%x unsupported=0x%x. Passing to host as is.",
+          guest_flags,
+          unknown_guest_flags);
   }
 
   return guest_flags;
+}
+
+int ToGuestOpenFlags(int host_flags) {
+  int unknown_host_flags = host_flags & ~kCompatibleOpenFlags;
+  if (unknown_host_flags) {
+    TRACE("Unrecognized host open flags: original=0x%x unsupported=0x%x. Passing to guest as is.",
+          host_flags,
+          unknown_host_flags);
+  }
+
+  return host_flags;
 }
 
 }  // namespace berberis
