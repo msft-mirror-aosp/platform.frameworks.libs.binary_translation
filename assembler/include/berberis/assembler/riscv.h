6514481e144261a2787bbe56732b017288e9aa72
David Coroian <davidcoroian@google.com>
2024-10-11 09:27:25 +0000

    Add riscv assembler jump pseudoinstructions
    Bug: 352784623
    Test: assembler_test
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:aeb3d96a21bcc9a186e7a6e02dd487ecb980c22d)
    Merged-In: I508ca789be3530a8d51bf2092950a7a60d24ae0a
    Change-Id: I508ca789be3530a8d51bf2092950a7a60d24ae0a

--- assembler/include/berberis/assembler/riscv.h
+++ assembler/include/berberis/assembler/riscv.h
@@ -1394,6 +1394,26 @@ inline void Assembler<DerivedAssemblerType>::Sgtz(Register arg0, Register arg1) 
   Slt(arg0, zero, arg1);
 }
 
+template <typename DerivedAssemblerType>
+inline void Assembler<DerivedAssemblerType>::J(JImmediate arg0) {
+  Jal(zero, arg0);
+}
+
+template <typename DerivedAssemblerType>
+inline void Assembler<DerivedAssemblerType>::Jal(JImmediate arg0) {
+  Jal(x1, arg0);
+}
+
+template <typename DerivedAssemblerType>
+inline void Assembler<DerivedAssemblerType>::Jr(Register arg0) {
+  Jalr(zero, arg0, 0);
+}
+
+template <typename DerivedAssemblerType>
+inline void Assembler<DerivedAssemblerType>::Jalr(Register arg0) {
+  Jalr(x1, arg0, 0);
+}
+
 }  // namespace riscv
 
 }  // namespace berberis
