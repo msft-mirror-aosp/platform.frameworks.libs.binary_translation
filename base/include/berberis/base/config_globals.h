f55bab142a6ea1e0048e4c01fbedaa64384c550a
Anthony Jones <anthonyjon@google.com>
2025-01-16 17:12:55 +0100

    Introduce 'disable intrinsic inlining' config flag
    Flag: EXEMPT NDK
    Bug: b/388117201
    Test: m berberis_all
    Change-Id: I4e583447fdb3b8f289cdd498888e8ae640f1eeda

--- base/include/berberis/base/config_globals.h
+++ base/include/berberis/base/config_globals.h
@@ -54,6 +54,7 @@ enum ConfigFlag {
   kEnableDisjointRegionsTranslation,
   kVerboseTranslation,
   kAccurateSigsegv,
+  kDisableIntrinsicInlining,
   // A convenience flag with no specific implied feature. Use it to conduct local experiments
   // without recompilation and without the need to add a new flag.
   kLocalExperiment,
