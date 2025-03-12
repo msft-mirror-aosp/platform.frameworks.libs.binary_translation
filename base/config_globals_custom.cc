dc206f1a38847302bbb73dafcb239fba3fcf5ce9
Lev Rumyantsev <levarum@google.com>
2025-03-12 08:40:55 -0700

    Add separate flags to control ir and addrs printing
    Often we don't need to print full IRs, and just the translated
    addrs is enough. So we add flags for both to use in the desired
    combination. It's unlikely that someone needs PrintIRs=true and
    PrintTranslatedAddrs=false, but we still keep two flags for simplicity.
    
    verbose-translation behaves as before.
    
    Flag: EXEMPT NDK
    Test: trigger prints in different combinations and check the resutl
    Bug: 395189011
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:5e90b8a7c13405f13a4203046be53a8db8dfd8f8)
    Merged-In: If4902aca8ce307808bf3c2938e31ac34d1f928cf
    Change-Id: If4902aca8ce307808bf3c2938e31ac34d1f928cf

--- base/config_globals_custom.cc
+++ base/config_globals_custom.cc
@@ -43,6 +43,10 @@ std::string ToString(ConfigFlag flag) {
       return "enable-disjoint-regions-translation";
     case kDisableIntrinsicInlining:
       return "disable-intrinsic-inlining";
+    case kPrintTranslatedAddrs:
+      return "print-translated-addrs";
+    case kPrintIRs:
+      return "print-irs";
     case kLocalExperiment:
       return "local-experiment";
     case kPlatformCustomCPUCapability:
