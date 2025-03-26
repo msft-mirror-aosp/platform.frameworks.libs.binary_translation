479da53cf0d63067b38d267930e6798d1e89315f
Richard Fung <richardfung@google.com>
2025-03-26 08:43:40 -0700

    Add code to check for READFLAGS optimization eligibility
    Flag: EXEMPT NDK
    Bug: 390481505
    Test: m berberis_all
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:e388eeb62b9c1e57555c2ff155e553a4130ee7f9)
    Merged-In: I37519b0b88bbc2862174a06630d6e14cc5a8fffd
    Change-Id: I37519b0b88bbc2862174a06630d6e14cc5a8fffd

--- backend/x86_64/read_flags_optimizer_test.cc
+++ backend/x86_64/read_flags_optimizer_test.cc
@@ -16,8 +16,6 @@
 
 #include "gtest/gtest.h"
 
-#include <tuple>
-
 #include "berberis/backend/x86_64/read_flags_optimizer.h"
 
 #include "berberis/backend/common/machine_ir.h"
@@ -31,18 +29,39 @@ namespace berberis::x86_64 {
 
 namespace {
 
-std::tuple<MachineBasicBlock*, MachineBasicBlock*, MachineBasicBlock*, MachineBasicBlock*>
-BuildBasicLoop(MachineIR* machine_ir) {
+struct TestLoop {
+  MachineBasicBlock* preloop;
+  MachineBasicBlock* loop_head;
+  MachineBasicBlock* loop_exit;
+  MachineBasicBlock* postloop;
+  MachineBasicBlock* successor;
+  MachineBasicBlock* succ_postloop;
+  MachineReg flags_reg;
+  // Iterator which points to the READFLAGS instruction.
+  MachineInsnList::iterator readflags_it;
+};
+
+TestLoop BuildBasicLoop(MachineIR* machine_ir) {
   x86_64::MachineIRBuilder builder(machine_ir);
 
+  // bb0 -> bb1 -> bb2 -> bb3
+  //         ^       |
+  //         |----- bb4 -> bb5
   auto bb0 = machine_ir->NewBasicBlock();
   auto bb1 = machine_ir->NewBasicBlock();
   auto bb2 = machine_ir->NewBasicBlock();
   auto bb3 = machine_ir->NewBasicBlock();
+  auto bb4 = machine_ir->NewBasicBlock();
+  auto bb5 = machine_ir->NewBasicBlock();
   machine_ir->AddEdge(bb0, bb1);
   machine_ir->AddEdge(bb1, bb2);
-  machine_ir->AddEdge(bb2, bb1);
   machine_ir->AddEdge(bb2, bb3);
+  machine_ir->AddEdge(bb2, bb4);
+  machine_ir->AddEdge(bb4, bb1);
+  machine_ir->AddEdge(bb4, bb5);
+
+  auto flags0 = machine_ir->AllocVReg();
+  auto flags1 = machine_ir->AllocVReg();
 
   builder.StartBasicBlock(bb0);
   builder.Gen<PseudoBranch>(bb1);
@@ -50,12 +69,25 @@ BuildBasicLoop(MachineIR* machine_ir) {
   builder.Gen<PseudoBranch>(bb2);
 
   builder.StartBasicBlock(bb2);
-  builder.Gen<PseudoCondBranch>(CodeEmitter::Condition::kZero, bb1, bb3, kMachineRegFLAGS);
+  builder.Gen<AddqRegReg>(machine_ir->AllocVReg(), machine_ir->AllocVReg(), kMachineRegFLAGS);
+  builder.Gen<PseudoReadFlags>(PseudoReadFlags::kWithOverflow, flags0, kMachineRegFLAGS);
+  builder.Gen<PseudoCopy>(flags1, flags0, 8);
+  builder.Gen<PseudoCondBranch>(CodeEmitter::Condition::kZero, bb3, bb4, kMachineRegFLAGS);
+  bb2->live_out().push_back(flags1);
 
   builder.StartBasicBlock(bb3);
   builder.Gen<PseudoJump>(kNullGuestAddr);
 
-  return {bb0, bb1, bb2, bb3};
+  builder.StartBasicBlock(bb4);
+  builder.Gen<PseudoCondBranch>(CodeEmitter::Condition::kZero, bb1, bb5, kMachineRegFLAGS);
+
+  builder.StartBasicBlock(bb5);
+  builder.Gen<PseudoJump>(kNullGuestAddr);
+
+  auto insn_it = std::next(bb2->insn_list().begin());
+  CHECK_EQ((*insn_it)->opcode(), kMachineOpPseudoReadFlags);
+
+  return {bb0, bb1, bb2, bb3, bb4, bb5, flags1, insn_it};
 }
 
 TEST(MachineIRReadFlagsOptimizer, CheckRegsUnusedWithinInsnRangeAddsReg) {
@@ -201,16 +233,16 @@ TEST(MachineIRReadFlagsOptimizer, CheckSuccessorNodeFailsIfUsingRegisters) {
   MachineReg flags = machine_ir.AllocVReg();
   ArenaVector<MachineReg> regs({flags}, machine_ir.arena());
 
-  auto [preloop, loop_head, loop_exit, postloop] = BuildBasicLoop(&machine_ir);
-  loop_exit->live_in().push_back(flags);
-  loop_exit->insn_list().insert(loop_exit->insn_list().begin(),
-                                machine_ir.NewInsn<MovqRegImm>(flags, 123));
+  auto testloop = BuildBasicLoop(&machine_ir);
+  testloop.loop_exit->live_in().push_back(flags);
+  testloop.loop_exit->insn_list().insert(testloop.loop_exit->insn_list().begin(),
+                                         machine_ir.NewInsn<MovqRegImm>(flags, 123));
 
   ASSERT_EQ(x86_64::CheckMachineIR(machine_ir), x86_64::kMachineIRCheckSuccess);
 
   auto loop_tree = BuildLoopTree(&machine_ir);
   auto loop = loop_tree.root()->GetInnerloopNode(0)->loop();
-  ASSERT_FALSE(CheckSuccessorNode(loop, loop_exit, regs));
+  ASSERT_FALSE(CheckSuccessorNode(loop, testloop.loop_exit, regs));
 }
 
 TEST(MachineIRReadFlagsOptimizer, CheckSuccessorNodeFailsIfNotExit) {
@@ -240,6 +272,7 @@ TEST(MachineIRReadFlagsOptimizer, CheckSuccessorNodeFailsIfNotExit) {
 
   auto loop_tree = BuildLoopTree(&machine_ir);
   auto loop = loop_tree.root()->GetInnerloopNode(0)->loop();
+
   // Should fail because not an exit node.
   ASSERT_FALSE(CheckSuccessorNode(loop, bb2, regs));
 }
@@ -250,20 +283,17 @@ TEST(MachineIRReadFlagsOptimizer, CheckSuccessorNodeInEdges) {
   x86_64::MachineIR machine_ir(&arena);
   x86_64::MachineIRBuilder builder(&machine_ir);
 
-  MachineReg flags = machine_ir.AllocVReg();
-  ArenaVector<MachineReg> regs({flags}, machine_ir.arena());
-
-  auto [preloop, loop_head, loop_exit, postloop] = BuildBasicLoop(&machine_ir);
-
+  auto testloop = BuildBasicLoop(&machine_ir);
   auto loop_tree = BuildLoopTree(&machine_ir);
   auto loop = loop_tree.root()->GetInnerloopNode(0)->loop();
+  ArenaVector<MachineReg> regs({testloop.flags_reg}, machine_ir.arena());
 
   ASSERT_EQ(x86_64::CheckMachineIR(machine_ir), x86_64::kMachineIRCheckSuccess);
 
-  loop_exit->live_in().push_back(flags);
-  ASSERT_TRUE(CheckSuccessorNode(loop, loop_exit, regs));
-  machine_ir.AddEdge(preloop, loop_exit);
-  ASSERT_FALSE(CheckSuccessorNode(loop, loop_exit, regs));
+  testloop.successor->live_in().push_back(testloop.flags_reg);
+  ASSERT_TRUE(CheckSuccessorNode(loop, testloop.successor, regs));
+  machine_ir.AddEdge(testloop.preloop, testloop.successor);
+  ASSERT_FALSE(CheckSuccessorNode(loop, testloop.successor, regs));
 }
 
 // regs should not be live_in to other loop nodes.
@@ -276,35 +306,33 @@ TEST(MachineIRReadFlagsOptimizer, CheckSuccessorNodeLiveIn) {
   MachineReg flags1 = machine_ir.AllocVReg();
   ArenaVector<MachineReg> regs({flags0}, machine_ir.arena());
 
-  auto [preloop, loop_head, loop_exit, postloop] = BuildBasicLoop(&machine_ir);
+  auto testloop = BuildBasicLoop(&machine_ir);
 
-  loop_exit->live_in().push_back(flags0);
+  testloop.loop_exit->live_in().push_back(flags0);
+  testloop.loop_exit->insn_list().insert(testloop.loop_exit->insn_list().begin(),
+                                         machine_ir.NewInsn<PseudoCopy>(flags1, flags0, 8));
 
-  loop_exit->insn_list().insert(loop_exit->insn_list().begin(),
-                                machine_ir.NewInsn<PseudoCopy>(flags1, flags0, 8));
-
-  postloop->live_in().push_back(flags1);
-
+  testloop.postloop->live_in().push_back(flags1);
   ASSERT_EQ(x86_64::CheckMachineIR(machine_ir), x86_64::kMachineIRCheckSuccess);
 
   auto loop_tree = BuildLoopTree(&machine_ir);
   auto loop = loop_tree.root()->GetInnerloopNode(0)->loop();
 
-  ASSERT_TRUE(CheckSuccessorNode(loop, loop_exit, regs));
+  ASSERT_TRUE(CheckSuccessorNode(loop, testloop.loop_exit, regs));
   // Remove flags1.
   regs.pop_back();
 
   // Make sure we fail if flags0 is live_in of another loop node.
-  loop_head->live_in().push_back(flags0);
-  ASSERT_FALSE(CheckSuccessorNode(loop, loop_exit, regs));
+  testloop.successor->live_in().push_back(flags0);
+  ASSERT_FALSE(CheckSuccessorNode(loop, testloop.loop_exit, regs));
 
   // Reset state.
-  loop_head->live_in().pop_back();
+  testloop.successor->live_in().pop_back();
   regs.pop_back();
 
   // Make sure that we check live_in after CheckRegsUnusedWithinInsnRange.
-  loop_head->live_in().push_back(flags1);
-  ASSERT_FALSE(CheckSuccessorNode(loop, loop_exit, regs));
+  testloop.successor->live_in().push_back(flags1);
+  ASSERT_FALSE(CheckSuccessorNode(loop, testloop.loop_exit, regs));
 }
 
 // Helper function to check that two instructions are the same.
@@ -341,6 +369,132 @@ TEST(MachineIRReadFlagsOptimizer, GetInsnGen) {
       &machine_ir,
       machine_ir.NewInsn<PseudoReadFlags>(
           PseudoReadFlags::kWithOverflow, machine_ir.AllocVReg(), kMachineRegFLAGS));
+}
+
+// Tests that IsEligibleReadFlags makes sure the flag register isn't used in the
+// exit node.
+TEST(MachineIRReadFlagsOptimizer, IsEligibleReadFlagChecksFlagsNotUsedInExitNode) {
+  Arena arena;
+  x86_64::MachineIR machine_ir(&arena);
+  auto testloop = BuildBasicLoop(&machine_ir);
+
+  ASSERT_EQ(x86_64::CheckMachineIR(machine_ir), x86_64::kMachineIRCheckSuccess);
+  auto loop_tree = BuildLoopTree(&machine_ir);
+  auto res = IsEligibleReadFlag(&machine_ir,
+                                loop_tree.root()->GetInnerloopNode(0)->loop(),
+                                testloop.loop_exit,
+                                testloop.readflags_it);
+  ASSERT_TRUE(res.has_value());
+
+  testloop.loop_exit->insn_list().push_back(
+      machine_ir.NewInsn<PseudoWriteFlags>(testloop.flags_reg, kMachineRegFLAGS));
+  res = IsEligibleReadFlag(&machine_ir,
+                           loop_tree.root()->GetInnerloopNode(0)->loop(),
+                           testloop.loop_exit,
+                           testloop.readflags_it);
+  ASSERT_FALSE(res.has_value());
+}
+
+// Tests that IsEligibleReadFlags checks post loop node.
+TEST(MachineIRReadFlagsOptimizer, IsEligibleReadFlagChecksPostloopNode) {
+  Arena arena;
+  x86_64::MachineIR machine_ir(&arena);
+  auto testloop = BuildBasicLoop(&machine_ir);
+  MachineReg flags_copy = machine_ir.AllocVReg();
+
+  testloop.postloop->live_in().push_back(testloop.flags_reg);
+  testloop.postloop->insn_list().push_front(
+      machine_ir.NewInsn<PseudoCopy>(flags_copy, testloop.flags_reg, 8));
+
+  ASSERT_EQ(x86_64::CheckMachineIR(machine_ir), x86_64::kMachineIRCheckSuccess);
+  auto loop_tree = BuildLoopTree(&machine_ir);
+  auto res = IsEligibleReadFlag(&machine_ir,
+                                loop_tree.root()->GetInnerloopNode(0)->loop(),
+                                testloop.loop_exit,
+                                testloop.readflags_it);
+  ASSERT_TRUE(res.has_value());
+
+  // Make postloop node fail by having the copy be live_out.
+  testloop.postloop->live_out().push_back(testloop.flags_reg);
+  res = IsEligibleReadFlag(&machine_ir,
+                           loop_tree.root()->GetInnerloopNode(0)->loop(),
+                           testloop.loop_exit,
+                           testloop.readflags_it);
+  ASSERT_FALSE(res.has_value());
+}
+
+// Tests that IsEligibleReadFlags checks loop successor node.
+TEST(MachineIRReadFlagsOptimizer, IsEligibleReadFlagChecksSuccessorNode) {
+  Arena arena;
+  x86_64::MachineIR machine_ir(&arena);
+  auto testloop = BuildBasicLoop(&machine_ir);
+
+  ASSERT_EQ(x86_64::CheckMachineIR(machine_ir), x86_64::kMachineIRCheckSuccess);
+  auto loop_tree = BuildLoopTree(&machine_ir);
+  auto res = IsEligibleReadFlag(&machine_ir,
+                                loop_tree.root()->GetInnerloopNode(0)->loop(),
+                                testloop.loop_exit,
+                                testloop.readflags_it);
+  ASSERT_TRUE(res.has_value());
+
+  // Make successor fail by accessing the register.
+  testloop.successor->live_in().push_back(testloop.flags_reg);
+  testloop.successor->insn_list().push_front(
+      machine_ir.NewInsn<PseudoWriteFlags>(machine_ir.AllocVReg(), testloop.flags_reg));
+  res = IsEligibleReadFlag(&machine_ir,
+                           loop_tree.root()->GetInnerloopNode(0)->loop(),
+                           testloop.loop_exit,
+                           testloop.readflags_it);
+  ASSERT_FALSE(res.has_value());
+}
+
+// Tests that IsEligibleReadFlags checks successor's postloop node.
+TEST(MachineIRReadFlagsOptimizer, IsEligibleReadFlagChecksSuccPostLoopNode) {
+  Arena arena;
+  x86_64::MachineIR machine_ir(&arena);
+  auto testloop = BuildBasicLoop(&machine_ir);
+  MachineReg flags_copy = machine_ir.AllocVReg();
+
+  testloop.successor->live_in().push_back(testloop.flags_reg);
+  testloop.successor->insn_list().push_front(
+      machine_ir.NewInsn<PseudoCopy>(flags_copy, testloop.flags_reg, 8));
+  testloop.successor->live_out().push_back(flags_copy);
+  testloop.succ_postloop->live_in().push_back(flags_copy);
+
+  ASSERT_EQ(x86_64::CheckMachineIR(machine_ir), x86_64::kMachineIRCheckSuccess);
+  auto loop_tree = BuildLoopTree(&machine_ir);
+  auto res = IsEligibleReadFlag(&machine_ir,
+                                loop_tree.root()->GetInnerloopNode(0)->loop(),
+                                testloop.loop_exit,
+                                testloop.readflags_it);
+  ASSERT_TRUE(res.has_value());
+
+  // succ_postloop should fail if it lets flags_copy be live_out.
+  testloop.succ_postloop->live_out().push_back(flags_copy);
+  res = IsEligibleReadFlag(&machine_ir,
+                           loop_tree.root()->GetInnerloopNode(0)->loop(),
+                           testloop.loop_exit,
+                           testloop.readflags_it);
+  ASSERT_FALSE(res.has_value());
+}
+
+// Tests that IsEligibleReadFlags returns the right instruction.
+TEST(MachineIRReadFlagsOptimizer, IsEligibleReadFlagReturnsSetter) {
+  Arena arena;
+  x86_64::MachineIR machine_ir(&arena);
+  auto testloop = BuildBasicLoop(&machine_ir);
+  testloop.loop_exit->insn_list().push_front(
+      machine_ir.NewInsn<SubqRegImm>(machine_ir.AllocVReg(), 121, testloop.flags_reg));
+
+  ASSERT_EQ(x86_64::CheckMachineIR(machine_ir), x86_64::kMachineIRCheckSuccess);
+  auto loop_tree = BuildLoopTree(&machine_ir);
+
+  auto insn_it = std::next(testloop.loop_exit->insn_list().begin(), 2);
+  ASSERT_EQ((*insn_it)->opcode(), kMachineOpPseudoReadFlags);
+  auto res = IsEligibleReadFlag(
+      &machine_ir, loop_tree.root()->GetInnerloopNode(0)->loop(), testloop.loop_exit, insn_it);
+  ASSERT_TRUE(res.has_value());
+  ASSERT_EQ(res.value()->opcode(), kMachineOpAddqRegReg);
 }
 
 TEST(MachineIRReadFlagsOptimizer, FindFlagSettingInsn) {
